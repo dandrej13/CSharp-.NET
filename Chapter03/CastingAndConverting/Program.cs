fdfafa458e59d6f7a712c48b0a9e0901ab16de94
dandrej13 <dimitrievsk.andrej@hotmail.com>
2022-09-22 15:09:50 +0200

    chapter03

--- Chapter03/CastingAndConverting/Program.cs
+++ Chapter03/CastingAndConverting/Program.cs
@@ -13,4 +13,29 @@ double g = 9.8;
     int h = ToInt32(g);
 WriteLine($"g is {g} and h is {h}");
 
+//alocate array of 128 bytes
+byte[] binaryObject = new byte[128];
 
+//populate array with random bytes
+(new Random()).NextBytes(binaryObject);
+
+WriteLine("Binary Object as bytes");
+
+for (int i = 0; i < binaryObject.Length; i++)
+{
+    Write($"{binaryObject[i]:X} ");
+};
+
+WriteLine();
+
+//convert to Base64 string and output the text
+string encoded = ToBase64String(binaryObject);
+WriteLine($"Binary object into Base64 is: {encoded} ");
+
+int age = int.Parse("23");
+
+DateTime birthday = DateTime.Parse("13 December 1998");
+
+WriteLine($"I was born {age} years ago.");
+WriteLine($"My birthday is {birthday}.");
+WriteLine($"My birthday is {birthday:D}.");
---
+++ Chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+WriteLine("Before parsing");
+Write("What is your age? ");
+string? input = ReadLine();
+string message = "NaN";
+try
+{
+    int age = int.Parse(input);
+    Console.WriteLine($"You are {age} years old" );
+}
+catch(Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {message}");
+}
+WriteLine("After Parsing");
