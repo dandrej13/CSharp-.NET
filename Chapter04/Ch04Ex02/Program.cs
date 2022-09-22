10575b386376a90797f5903535ef53f37c290d42
dandrej13 <dimitrievsk.andrej@hotmail.com>
2022-09-22 15:47:24 +0200

    odmor

---
+++ Chapter04/Ch04Ex02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ch04Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string comparison;
+            Console.WriteLine("Enter a number");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a < b)
+            {
+                comparison = "smaller than";
+            }
+            else if (a == b)
+            {
+                comparison = "equal to";
+            }
+            else
+            {
+                comparison = "greater than";
+            }
+
+            Console.WriteLine($"{a} is {comparison} {b}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
