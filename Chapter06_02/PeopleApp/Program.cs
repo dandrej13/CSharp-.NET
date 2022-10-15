c8da36e5abd4ff655749de6e4a9272288cae9353
dandrej13 <dimitrievsk.andrej@hotmail.com>
2022-10-15 02:12:53 +0200

    old things

---
+++ Chapter06_02/PeopleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using PeopleLib;
+using static System.Console;
+
+Person harry = new Person() { Name = "Harry" };
+Person marry = new Person() { Name = "Marry" };
+Person jill = new Person() { Name = "Jill" };
+
+// instance method
+Person baby1 = marry.ProcreateWith(harry);
+baby1.Name = "Garry";
+
+// static method
+Person baby2 = Person.Procreate(harry, jill);
+baby2.Name = "Steve";
+
+//opreator
+Person baby3 = harry * marry;
+
+
+WriteLine($"{harry.Name} has {harry.Children.Count} children");
+WriteLine($"{marry.Name} has {marry.Children.Count} children");
+WriteLine($"{jill.Name} has {jill.Children.Count} children");
+WriteLine(
+    format: "{0}'s first child's name is \"{1}\".",
+    arg0: harry.Name,
+    arg1: harry.Children[1].Name
+    );
---
+++ Chapter06_02/PeopleLib/Person.cs
@@ -0,0 +1,55 @@
+﻿namespace PeopleLib
+{
+    public class Person : object
+    {
+        public string? Name;
+        public DateTime DateOfBirth;
+        public string? FirstName;
+        public List<Person> Children = new();
+
+        public void WriteToConsole()
+        {
+            Console.WriteLine($"{Name} was born on {DateOfBirth:dddd}");
+        }
+
+        public static Person Procreate(Person p1, Person p2)
+        {
+            Person baby = new Person()
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+        }
+
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+
+        public static Person operator *(Person p1, Person p2)
+        {
+            return Person.Procreate(p1, p2);
+        }
+
+        //method with a local function
+
+        public static int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException(
+                    $"{nameof(number)} cannot be less than zero.");
+            }
+            return localFactorial(number);
+
+            int localFactorial(int localNumber)// local function
+            {
+                if (localNumber < 1) return 1;
+                return localNumber * localFactorial(localNumber - 1);
+            }
+        }
+
+    }
+}
